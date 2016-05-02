5ce9fbefa2a106cbb6629b5c29fd4ff0c5c7c819
Dmitriy Kubyshkin <dmitriy.kubyshkin@zalando.de>
2016-05-02 23:52:29 +0200

    Added node attribute handling with callbacks. Some small housekeeping.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const Registry = require('../lib/registry');
+const Registry = require('../lib/hook');
 const mendeleev = require('../lib/index');
 const assert = require('assert');
 
 describe('mendeleev', () => {
-    it('should be an instance of Registry', () => {
+    it('should be an instance of Hook', () => {
         assert(mendeleev instanceof Registry);
     });
 });
